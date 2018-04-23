f434806ec5ba6b3132ca409afc8a4481372e0d87
mandar-raskar <30428372+mandar-raskar@users.noreply.github.com>
2018-04-23 10:24:30 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,117 +1,15 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-
-Genre = require("./models/genre");
-Book = require("./models/books");
+var express = require('express');
 
 var app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+var server = require("/routes/server");
 
-mongoose.connect("mongodb://localhost:27017/book");
-var db = mongoose.connection;
+app.use('/server', server);
 
-app.get("/", function(req,res){
-  res.send("hello");
+app.use('/',function(req,res){
+  res.status(404).json({
+    "message":"error found"
+  })
 });
 
-app.get("/api/genres",function(req,res){
-  Genre.getgenres(function(err,genre){
-    if(err){
-    throw err;
-  }
-  res.json(genre);
-  });
-});
-
-
-app.post("/api/genres",function(req,res){
-  var genre =req.body;
-  Genre.addgenres(genre,function(err,genre){
-    if(err){
-    throw err;
-  }
-  res.json(genre);
-  });
-});
-
-app.put("/api/genres/:_id", function(req,res){
-  var id = req.params._id;
-  var genre = req.body;
-  Genre.updategenre(id,genre,{},function(err,genre){
-    if (err){
-    throw err;
-  }
-  res.json(genre);
-});
-});
-
-app.delete("/api/genres/:_id", function(req,res){
-  var id = req.params._id;
-  Genre.removegenre(id, function(err,genre){
-    if(err){
-      throw err;
-      }
-    res.json(genre);
-  });
-});
-
-
-  app.get("/api/books",function(req,res){
-    Book.getbooks(function(err,books){
-      if(err){
-      throw err;
-    }
-    res.json(books);
-  });
-
-});
-
-app.get("/api/books/:_id",function(req,res){
-  Book.getbookById(req.params._id,function(err,bookId){
-    if(err){
-    throw err;
-  }
-  res.json(bookId);
-});
-
-});
-
-app.post("/api/books",function(req,res){
-  var book = req.body;
-  Book.addbook(book, function(err,book){
-    if (err){
-      throw err;
-    }
-    res.json(book);
-  });
-});
-
-app.put("/api/books/:_id",function(req,res){
-  var id = req.params._id;
-  var book = req.body;
-  Book.updatebook(id,book,{},function(err,book){
-    if(err){
-      throw err;
-    }
-    res.json(book);
-  });
-});
-
-app.delete("/api/books/:_id",function(req,res){
-  var id = req.params._id;
-  Book.removebook(id, function(err,book){
-    if (err){
-      throw err;
-    }
-    res.json(book);
-  });
-});
-
-
-
-
-app.listen(3004);
-console.log("server is running on port no. 3000");
+module.exports = app();
--- server.js
+++ server.js
@@ -1,30 +1,17 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+var body-parser = require('body-parser');
 
-
-
-var index = require('./routes/index');
-var tasks = require('./routes/tasks');
-
-var port = 5000;
-var app = express();
-
-
-app.set('views',path.join(__dirname, 'views'));
-app.set('view engine','ejs');
-app.engine('html',require('ejs').renderFile);
-
-app.use(express.static(path.join(__dirname,'client')));
-
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-
-
-
-app.use('/',index);
-app.use('/api',tasks);
-
-app.listen(port, function(){
-  console.log("server is running on "+ port);
+app.get("/",function(req,res){
+  res.status(400).json(
+    {
+      "message":"Its being executed"
+    }
+  )
 });
+
+app.post("/",function(req,res){
+  var data =
+  {
+    name : req.body.uname;
+    pass : req.body.upass;
+  }
+})
